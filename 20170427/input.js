fe0a72e66fd7086f3aa102d88be0c4f6d821abc7
Kyungyong Lee <leeky@kookmin.ac.kr>
2017-04-29 23:42:53 +0900

    code for 20170427

---
+++ 20170427/input.js
@@ -0,0 +1,2 @@
+var name = prompt("What is your name: ");
+console.log("name is ", name);
---
+++ 20170427/string.js
@@ -0,0 +1,6 @@
+var a = "String with double quote";
+var b = 'string with single quote';
+var c = 'double "quote" within single quote';
+var d = "single 'quote' within double quote";
+
+
