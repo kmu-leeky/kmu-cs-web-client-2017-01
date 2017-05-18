a9d88d03b9e6372452015243e9969c65c91de203
Kyungyong Lee <leeky@kookmin.ac.kr>
2017-05-18 19:55:10 +0900

    adding code for 20170518

---
+++ 20170518/window.js
@@ -0,0 +1,7 @@
+console.log(window.location);
+console.log(window.location.href);
+window.alert("alert message");
+console.log(window.document.body);
+console.log(window.screen);
+console.log(document.styleSheets[0].cssRules[0].style.cssText);
+
