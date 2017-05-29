669acdbe7d69404a923648799e68764ed075f3c9
Kyungyong Lee <leeky@kookmin.ac.kr>
2017-05-29 21:36:34 +0900

    adding code for 20170529

---
+++ 20170529/jquery_siblings.js
@@ -0,0 +1,9 @@
+$('#target').prev().attr('style', 'color:red');
+$('#target').prevAll().attr('style', 'color:red');
+$('#target').prevUntil('.stop').attr('style', 'color:red');
+
+$('#target').next().attr('style', 'color:green');
+$('#target').nextAll().attr('style', 'color:green');
+$('#target').nextUntil('.stop').attr('style', 'color:green');
+
+
